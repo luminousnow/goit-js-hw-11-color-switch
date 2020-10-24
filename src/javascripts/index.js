270e3a1455750665a1f66ef6fc8528a785b469f9
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2020-10-24 20:58:18 +0300

    Update index.js

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -13,38 +13,33 @@ const max = Number(colors.length - 1);
 const refs = {
   buttons: document.querySelector('.js-buttons'),
   body: document.querySelector('body'),
-}
+};
 
 let interval = '';
 
-
 refs.buttons.addEventListener('click', onBtnClick);
 
-
 function onBtnClick(e) {
-  
   if (e.target.dataset.action === 'start') {
     // робить кнопку start неактивною
     e.target.disabled = true;
-    
+
     // задає інтервал
     interval = setInterval(changeColor, 1000);
-    
-    console.log('started');
 
-  };
-  
+    console.log('started');
+  }
+
   if (e.target.dataset.action === 'stop') {
     // робить кнопку start активною
     const btnStart = document.querySelector('[data-action="start"]');
     btnStart.disabled = false;
-    
+
     // очищує інтервал
     clearInterval(interval);
-    
+
     console.log('stoped');
-    
-  };
+  }
 }
 
 // генерує випадкове число в заданому діапазоні
